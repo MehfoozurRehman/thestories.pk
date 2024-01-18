821039884651f819cbac4c3322d2a8b0522f2551
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-18 13:59:05 +0500

    base nextjs with mongoose

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import dbConnect from "@/lib/dbConnect";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "theStories - Pakistan News",
+  description: "A news site about pakistan",
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  await dbConnect();
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
