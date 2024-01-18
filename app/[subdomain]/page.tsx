ddba0fe6afd750120c29c5063aa3f5003ac74355
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-18 16:39:56 +0500

    Update page.tsx

--- app/[subdomain]/page.tsx
+++ app/[subdomain]/page.tsx
@@ -1,3 +1,23 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+export default function Login() {
+  return (
+    <div className="login__container">
+      <form action="" className="login__container__form">
+        <h1 className="login__container__form__heading">
+          Login to your account
+        </h1>
+        <p className="login__container__form__info">
+          Enter your email and password to login
+        </p>
+        <div className="login__container__form__input">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="login__container__form__input">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button className="login__container__form__button">Login</button>
+      </form>
+    </div>
+  );
 }
